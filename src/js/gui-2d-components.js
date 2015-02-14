1e4362a6b3a902a09f0c5ceda8c3f1977693ead6
Ska <sergiosicari@gmail.com>
2015-02-14 10:21:21 +0100

    selector component now start to work

--- src/js/gui-2d-components.js
+++ src/js/gui-2d-components.js
@@ -33,7 +33,7 @@ Gui2DComponents.directive('rotative', ['$document', function($document){
             function bindElementMove() {
                 element.bind('mousedown', function (event) {
                     // Prevent default dragging of selected content
-                    console.log("binding element to move.");
+                    //console.log("binding element to move.");
                     $document.bind('mousemove', mousemove);
                     $document.bind('mouseup', mouseup);
                 });
@@ -92,9 +92,10 @@ Gui2DComponents.directive('selector', ['$document', function($document){
 				content.css('color', $scope.color);
 				pie.css('background-color', $scope.color);
 				pie.css('border-color', $scope.color);
+				pie.addClass("zeroPositionLeft");
 			},
 			link: function(scope, element, attr, ngModel) {
-				var x = 0, y = 90, yReal = 0, color = 'white', value = 0;
+				var x = 0, y = 110, yReal = 0, color = 'white', value = 0;
 				function bindElementMove() {
 	                element.bind('mousedown', function (event) {
 	                    // Prevent default dragging of selected content
@@ -108,31 +109,41 @@ Gui2DComponents.directive('selector', ['$document', function($document){
 
            		function mousemove(event) {
            			var realStep = 360/parseInt(scope.position);
+           			var lastPos = realStep*parseInt(scope.position);
 		    		var prevY = element.attr('data-prevY');
-		    		// check max value (starting from 90deg the end is (360+90)deg then sub 5 to have a margin)
-		    		//if (y > 449) {y = 449;}
+		    		var checkPos = element.attr('data-checkPos');
+		    		if(typeof(checkPos)==="undefined")checkPos=0;
+		    		// check max value 
+		    		if (y > lastPos) {y = lastPos;}
 		    		// check min value
-		    		//if (y < 99) {y = 99;}
+		    		if (y < 110) {y = 110;}
 		    		// mouse goes down
 		    		if (event.pageY < prevY) {
-		    			y=y+10;
+    					checkPos=parseInt(checkPos)+10;
+    					if(checkPos>(realStep-1)){
+    						y=y+realStep;
+    						checkPos=0;
+    					}
 		    			//value = value + parseInt(scope.step);
    					// mouse goes up
     				} else {
-    					y=y-10;
+    					checkPos=parseInt(checkPos)+10;
+    					if(checkPos>(realStep-1)){
+    						y=y-realStep;
+    						checkPos=0;
+    					}
     					//value = value - parseInt(scope.step);
     				}
-    				console.log(y);	
+    				//console.log(y);	
+    				element.attr('data-prevY', event.pageY);
+    				element.attr('data-checkPos', checkPos);
 		      		var pie = element.find('pie');
-		      		if (y > 270) {
-    					yReal = y - 270 + 81;
-    					color = scope.color;
-    				} else {
-    					yReal = y;
-    					color = 'white';
-    				}
+
+    				yReal = y;
+    				color = 'white';
+    				
 		      		//ngModel.$setViewValue(value);
-		      		pie.css('background-image','linear-gradient('+yReal+'deg, transparent 50%, '+color+' 50%),linear-gradient(90deg, white 50%, transparent 50%');
+		      		pie.css('background-image','linear-gradient('+yReal+'deg, transparent 50%, '+color+' 50%),linear-gradient('+(yReal+4)+'deg, white 50%, transparent 50%');
            		}
 
            		function mouseup() {
