4af13cede96e12a70c6916303aada015d3ef5ccd
sicari <sergio>
2015-02-16 17:21:20 +0100

    add behavior testing with karma/jasmine

---
+++ test/gui-2d-components.spec.js
@@ -0,0 +1,35 @@
+'use strict';
+
+describe('Gui2DComponents: selector', function() {
+	var element, scope, html, $httpBackend;
+	beforeEach(module('templates'));
+	beforeEach(module('Gui2DComponents'));
+
+	beforeEach(inject(function($rootScope, $compile) {
+
+	    scope = $rootScope.$new();
+
+	    html = '<selector id="due" position="{{position}}" color="{{color}}" label="{{label}}"></selector>';
+	    element = html;
+
+	    scope.position = 5;
+	    scope.color = "blue";
+	    scope.label = "type";
+	    scope.model = "type";
+
+	    element = $compile(element)(scope);
+	    scope.$digest();
+	}));
+
+	describe('test first position', function() {
+		it("should be position=5, color=blue, label=type", function() {
+			var ele = element.isolateScope();
+    		expect(ele.position).toBe('5');
+    		expect(ele.color).toBe('blue');
+    		expect(ele.label).toBe('type');
+
+  		});
+	});
+
+
+});
