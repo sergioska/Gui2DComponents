d8ca201da7cd83c564eec63d0565f7dcea9638f4
sicari <sergio>
2015-02-19 16:55:25 +0100

    demo also in sidebar

--- javascripts/test.js
+++ javascripts/test.js
@@ -3,6 +3,11 @@ var testGui = angular.module('testGui', ['Gui2DComponents']);
 testGui.controller('MainController', function($scope){
 	$scope.init = function() {
 		$scope.gain = 0;
+		$scope.hi = 0;
+		$scope.low = 0;
+		$scope.type = 0;
+		$scope.subtype = 0;
+		$scope.another = 0;
 		$scope.switchStatus=0;
 	}
 	$scope.$watch(function() {
